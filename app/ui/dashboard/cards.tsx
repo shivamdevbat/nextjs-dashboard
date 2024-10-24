88de95ee3834a0b99d608f4fb56c3ef235a146ad
shivamdevbat <shivamdwivedi1211@gmail.com>
2024-10-24 20:02:44 +0530

    changes-2

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,5 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from "@heroicons/react/24/outline";
+import { lusitana } from "@/app/ui/fonts";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -16,16 +11,14 @@ const iconMap = {
 export default async function CardWrapper() {
   return (
     <>
-      {/* NOTE: Uncomment this code in Chapter 9 */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
-      <Card title="Pending" value={totalPendingInvoices} type="pending" />
-      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      {
+        <>
+          <Card title="Collected" value={totalPaidInvoices} type="collected" />
+          <Card title="Pending" value={totalPendingInvoices} type="pending" />
+          <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+          <Card title="Total Customers" value={numberOfCustomers} type="customers" />
+        </>
+      }
     </>
   );
 }
@@ -37,7 +30,7 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
